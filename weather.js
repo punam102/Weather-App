b27e0a8058c5e84eb2ff3835b88b2ccfa4862e8e
punam102 <jagtappunam129@gmail.com>
2023-01-06 16:13:24 +0530

    completed

--- weather.js
+++ weather.js
@@ -1,21 +1,39 @@
 var searchValue = document.querySelector("#searchbox");
-searchValue.addEventListener("keypress", setFunc);
-function setFunc(e) {
-  if (e.keyCode == 13) {
-    getData(searchValue.value);
-  }
-}
-function getData(value) {
-  fetch(`https://api.openweather.org/data/2.5/weather?q=`+value+`&appid=2787bda66193d6b033a09da2fa95cd82
-    `)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (weather) {
-      document.querySelector("#city").innerHTML = weather.name;
-      document.querySelector('#temp').innerHTML = weather.main.temp;
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
+      searchValue.addEventListener("keypress", setFunc);
+      function setFunc(e) {
+        if (e.keyCode == 13) {
+          getData(searchValue.value);
+        }
+      }
+      function getData(value) {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=`+
+            value +
+            `&appid=a98a3edaa4e478948d9e2eb5cfcc8184
+                `
+        )
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (weather) {
+            document.querySelector('#city').innerHTML = weather.name;
+            document.querySelector('#temp').innerHTML = weather.main.temp;
+            document.querySelector('#min_max_temp').innerHTML =
+              'Min.' +
+              weather.main.temp_min +
+              ' / Max.' +
+              weather.main.temp_max;
+            document.querySelector('#weather').innerHTML =
+              weather.weather[0].main;
+            document.querySelector('#weather_desc').innerHTML =
+              weather.weather[0].description;
+            document.querySelector('#wind_dir_speed').innerHTML =
+              'Wind direction:' +
+              weather.wind.deg +
+              '/ Wind speed:' +
+              weather.wind.speed;
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      }
